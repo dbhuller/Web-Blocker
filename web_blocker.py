13af2962247ac3af6673288b76762196a399eef6
Deepinder Bhuller <dbhuller@mail.sfsu.edu>
2019-08-23 15:11:56 -0700

    Checks time, prints working hours if between 8am-8pm, else Non-Working hours

--- web_blocker.py
+++ web_blocker.py
@@ -1,1 +1,16 @@
+
+import time
+from datetime import datetime as dt
+
 # host file path: C:\Windows\System32\drivers\etc
+host_path = 'C:\Windows\System32\drivers\etc\hosts'
+redirect = '127.0.0.1'
+website_list = ['www.facebook.com', 'facebook.com', 'www.instagram.com', 'instagram.com']
+
+while True:
+    #check time
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print('Working Hours')
+    else:
+        print('Non-Working Hours')    
+    time.sleep(5)

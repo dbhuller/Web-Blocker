149be3196f9d9a7d1d019e39d6baf0356606f599
Deepinder Bhuller <dbhuller@mail.sfsu.edu>
2019-08-23 19:13:32 -0700

    used hosts_path to access actual host file and successfully blocked sites

--- web_blocker.py
+++ web_blocker.py
@@ -3,17 +3,17 @@ import time
 from datetime import datetime as dt
 
 # host file path: C:\Windows\System32\drivers\etc
-#host_path = 'C:\Windows\System32\drivers\etc\hosts'
-host_test = 'hosts_test'
+host_path = 'C:\Windows\System32\drivers\etc\hosts'
+#host_test = 'hosts_test'
 redirect = '127.0.0.1'
 website_list = ['www.facebook.com', 'facebook.com', 'www.instagram.com', 'instagram.com']
 
 while True:
     #check time
-    if dt(dt.now().year, dt.now().month, dt.now().day, 16) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print('Working Hours')
         #reading and printing the file
-        with open(host_test, 'r+') as file:
+        with open(host_path, 'r+') as file:
             content = file.read()
             for site in website_list:
                 if site in content:
@@ -21,7 +21,7 @@ while True:
                 else:
                     file.write(redirect + ' ' + site+'\n')
     else:
-        with open(host_test,'r+') as file:
+        with open(host_path,'r+') as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
